8c6424ceb35be1622cc8d0a0f31046a7045f0c65
Guilherme Montanholli <gui_montanholli@hotmail.com>
2025-01-08 23:29:21 -0300

    Primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade login', () => {
+
+it('Deve fazer login com sucesso', () =>{
+
+cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+cy.get('#username').type ('guilherme@teste.com.br')
+cy.get('#password').type ('teste123')
+cy.get('.woocommerce-form > .button').click()
+
+cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, guilherme (não é guilherme? Sair')
+
+})
+
+})
