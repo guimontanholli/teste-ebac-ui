84163d62a45b4b2a31e96c9116ba1784ac318845
Guilherme Montanholli <gui_montanholli@hotmail.com>
2025-01-09 00:04:01 -0300

    Testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,15 +2,36 @@
 
 describe('Funcionalidade login', () => {
 
-it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
 
-cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+it('Deve fazer login com sucesso', () =>{
 cy.get('#username').type ('guilherme@teste.com.br')
 cy.get('#password').type ('teste123')
 cy.get('.woocommerce-form > .button').click()
-
 cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, guilherme (não é guilherme? Sair')
 
-})
+});
+
+it('Deve exibir mensagem de erro ao inserir user inválido', () => {
+cy.get('#username').type ('guilher')
+cy.get('#password').type ('teste123')
+cy.get('.woocommerce-form > .button').click()
+//cy.get('.woocommerce-error > li').should('contain', 'Erro: O usuário guilher não está registrado neste site.')
+cy.get('.woocommerce-error').should('exist')
+
+});
+
+it('Deve exibir senha inválida', () => {
+cy.get('#username').type ('guilherme@teste.com.br')
+cy.get('#password').type ('teste')
+cy.get('.woocommerce-form > .button').click()
+cy.get('.woocommerce-error').should('exist')
+});
 
 })
