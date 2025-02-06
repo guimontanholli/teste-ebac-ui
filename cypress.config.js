73e1522d2ef4f70e10654bd56bffe84c9b4a000a
Guilherme Montanholli <gui_montanholli@hotmail.com>
2025-02-06 12:26:11 -0300

    exercicio cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'r5ps32',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
+    
   },
 });
